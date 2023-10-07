17bd66476aae14d5a5c6e3808a38d66e3a6a1230
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2023-10-07 22:39:34 +0700

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -48,12 +48,8 @@ HELP_MESSAGE = """Commands:
 /mode – Select chat mode
 """
 
-HELP_GROUP_CHAT_MESSAGE = """You can add bot to any <b>group chat</b> to help and entertain its participants!
-
-Instructions (see <b>video</b> below):
-1. Add the bot to the group chat
-2. Make it an <b>admin</b>, so that it can see messages (all other rights can be restricted)
-3. You're awesome!
+HELP_GROUP_CHAT_MESSAGE = """
+You're awesome!
 
 To get a reply from the bot in the chat – @ <b>tag</b> it or <b>reply</b> to its message.
 For example: "{bot_username} write a poem about Telegram"
